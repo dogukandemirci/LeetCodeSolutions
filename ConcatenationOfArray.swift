4fbb530fb9c40f097dcc8ac534ebae990e8d52a0
Dogukan Demirci <dogukandemirci@yahoo.com>
2023-01-02 22:13:08 +0300

    Added solution for "Concatenation of Array".

---
+++ ConcatenationOfArray.swift
@@ -0,0 +1,15 @@
+//
+//  ConcatenationOfArray.swift
+//  LeetCodeSolutions
+//
+//  Created by Dogukan Demirci on 2.01.2023.
+//
+// MARK: Question Link: https://leetcode.com/problems/concatenation-of-array/
+
+import Foundation
+
+class ConcatenationOfArray {
+    func getConcatenation(_ nums: [Int]) -> [Int] {
+        return nums+nums
+    }
+}
