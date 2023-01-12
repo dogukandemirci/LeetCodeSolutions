44e0ff5b0444cb1d7dab6160ecb3ad2ee2c73ac2
Dogukan Demirci <dogukandemirci@yahoo.com>
2023-01-12 23:48:12 +0300

    Added file for "Merge Two Sorted Lists".

---
+++ MergeTwoSortedLists.swift
@@ -0,0 +1,14 @@
+//
+//  MergeTwoSortedLists.swift
+//  
+//
+//  Created by Dogukan Demirci on 12.01.2023.
+//
+// MARK: https://leetcode.com/problems/merge-two-sorted-lists/
+
+import Foundation
+class MergeTwoSortedLists {
+    func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
+        
+    }
+}
